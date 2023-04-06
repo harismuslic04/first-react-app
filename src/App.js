7f8adf7e1173f5c87aa0f9cb42490dbad81ea8cf
harismuslic04 <harismuslic16@gmail.com>
2023-04-06 23:39:00 +0200

    react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import "./App.css";
-import Greeting from "./components/greetings/Greeting";
+import Greeting from "./components/Greetings/Greeting";
+import { Navbar } from "./components/Navbar/Navbar";
+import PersonCard from "./components/Cards/PersonCard/PersonCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="header">
-        <h1 style={{ color: "white" }}>Logo</h1>
-      </header>
-      <Greeting appName={"Our First App"} username={"Haris Muslic"}></Greeting>
-    </div>
+    //  React.createElement("p", {}, "Neki paragraf");
+    <>
+      {" "}
+      {/* Fragment - najcesce se koristi za wrappovanje */}
+      <div className="App">
+        <Navbar>{/* <p>Samo za primer</p> */}</Navbar>
+        <Greeting appName={"Our First App"} username={"Bakir Ujkanovic"} />
+        <PersonCard />
+      </div>
+    </>
   );
 }
 
--- src/components/greetings/Greeting.js
+++ src/components/greetings/Greeting.js
@@ -3,12 +3,12 @@ import "./Greeting.css";
 
 function Greeting(props) {
   console.log(props);
+  console.log(props.appName);
   return (
-    <div className="main">
-      <h2>Dobrodosli u {props.appName}</h2>
-      <h3>Hvala sto ste nas posetili, {props.username}</h3>
-    </div>
-    //
+    <main className="main">
+      <h2>Dobro dosli u {props.appName}!</h2>
+      <h3>Hvala sto ste nas posetili, {props.username}.</h3>
+    </main>
   );
 }
 
