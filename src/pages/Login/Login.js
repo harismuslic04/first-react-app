d9b91f176f5dde5923d1287ff27044a8caebc458
harismuslic04 <harismuslic16@gmail.com>
2023-05-09 20:24:44 +0200

    react

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import "./Login.css";
+import axios from "axios";
+import { BASE_URL } from "../../config/api";
+import { useNavigate } from "react-router-dom";
+
+export function Login() {
+  const navigation = useNavigate();
+  async function loginUser(data) {
+    try {
+      const user = await axios.post(`${BASE_URL}/users/login`, data);
+      const userInfo = await user.data;
+      console.log(userInfo);
+      // console.log(userInfo.token);
+      localStorage.setItem("token", userInfo.token);
+      navigation("/");
+    } catch (err) {
+      console.log(err.response.data.err);
+      localStorage.removeItem("token");
+    }
+  }
+
+  function handleClick(e) {
+    e.preventDefault();
+    loginUser({
+      email,
+      password,
+    });
+  }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  //   const [userInput, setUserInput] = useState({
+  //     email: "",
+  //     password:""
+  //   })
+
+  return (
+    <div className="cointener">
+      <form>
+        <h1>Login</h1>
+        <label className="label">Email</label>
+        <input
+          className="input"
+          type="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          placeholder="Email"
+          name="email"
+          required
+        ></input>
+        <label className="label">Password</label>
+        <input
+          className="input"
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          required
+        ></input>
+        <button id="login" onClick={handleClick}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
